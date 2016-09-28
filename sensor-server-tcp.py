84ce8a41f5cfa80603ee5d71d4b33df702cc5354
Luke Newman <lukenewman@bellsouth.net>
2016-09-27 22:18:24 -0400

    CRA working, beginning sensor

--- sensor-server-tcp.py
+++ sensor-server-tcp.py
@@ -8,47 +8,78 @@ import string		# for creating the challenge string
 import hashlib		# for MD5 hash
 import csv			# for parsing the password file
 
-# Default host, port, and filename
+# Default host, port, filename, and debug mode
 host = ''
-port = 12000
+port = 1234
 filename = ''
+debug = False
+
+# ==================== Helper Methods =====================
+
+# find_password will return the password corresponding to 'username'
+# 		or 'INVALID_CREDENTIALS' if credentials are invalid
+def find_password(username):
+	if debug:
+		print "Finding password for username '" + username + "'"
+	for credential in credentials:
+		if debug:
+			print str(credential)
+		if credential[0] == username:
+			return credential[1]
+	return "INVALID_CREDENTIALS"
+
+# trim_argument will trim the argument from the untrimmed form
+#		i.e. trim_argument('password=1234asdf', 'password') -> '1234asdf'
+def trim_argument(untrimmed, name):
+	# Find and validate '='
+    pos_equals = untrimmed.find("=")
+    if pos_equals == -1: return ''
+
+    # Validate argument name
+    argument_name = untrimmed[0:pos_equals]
+    if name not in argument_name:
+        return ''
+
+	# Return argument after '='
+    return untrimmed[pos_equals + 1:len(untrimmed)]
 
 # ============= Parse Command-Line Arguments ==============
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hp:f:",["port=", "file="])
+    opts, args = getopt.getopt(sys.argv[1:], "hdp:f:", ["debug=", "port=", "file="])
 except getopt.GetoptError:
-    print 'Please specify a port (-p) and password file (-f).'
+    print 'Please specify a port (-p) and password file (-f). Use -d for debug printing.'
     sys.exit()
 for opt, arg in opts:
-    if opt == '-h':
-        print 'Usage: ' + sys.argv[0] + ' -p <port> -f <password file>'
-        sys.exit()
-    elif opt in ("-p", "--port"):
-        port = int(arg)
-        # print 'Port: ' + arg
-    elif opt in ("-f", "--file"):
-        filename = arg
-        # print 'Filename: ' + filename
+	if opt == '-h':
+		print 'Usage: ' + sys.argv[0] + ' -p <port> -f <password file>'
+		sys.exit()
+	elif opt in ("-d", "--debug"):
+		debug = True
+	elif opt in ("-p", "--port"):
+		port = int(arg)
+		if debug:
+			print 'Port: ' + arg
+	elif opt in ("-f", "--file"):
+		filename = arg
+		if debug:
+			print 'Filename: ' + filename
+
+# ========================================================
 
 # ================= Parse Password File ==================
 
-password_file = open(filename)
-reader = csv.reader(password_file)
-credentials = list(reader)
-# credentials are stored in the following format:
+# Credentials are stored in the following format:
 #         [[username1, password1],
 #          [username2, password2],
 #          [username3, password3],
 #          ...]
 
-# find_password will return the password corresponding to 'username'
-# 		or 'INVALID_CREDENTIALS' if credentials are invalid
-def find_password(username):
-	for credential in credentials:
-		if credential[0] == username:
-			return credential[1]
-	return "INVALID_CREDENTIALS"
+password_file = open(filename)
+reader = csv.reader(password_file)
+credentials = list(reader)
+
+# ========================================================
 
 # ======== Create an INET, STREAMing socket (TCP) ========
 
@@ -58,9 +89,12 @@ except socket.error:
 	print 'Failed to create socket.'
 	sys.exit()
 
-print 'Socket created.'
+if debug:
+	print 'Socket created.'
 
-# =============== Bind the socket and host ===============
+# ========================================================
+
+# =============== Bind the Socket and Host ===============
 
 try:
 	s.bind((host, port))
@@ -68,9 +102,12 @@ except socket.error, msg:
 	print 'Bind failed. Error code: ' + str(msg[0]) + ' | Message: ' + msg[1]
 	sys.exit()
 
-print 'Socked bind complete.'
+if debug:
+	print 'Socked bind complete.'
 
-# ================== Listen on the port ==================
+# ========================================================
+
+# ================== Listen on the Port ==================
 
 try:
 	s.listen(port)
@@ -78,53 +115,141 @@ except socket.error, msg:
 	print 'Socket listen failed.  Error code: ' + str(msg[0]) + ' | Message: ' + msg[1]
 	sys.exit()
 
-print 'Socket listening on port ' + str(port)
+if debug:
+	print 'Socket listening on port ' + str(port)
 
-while 1:
+# ========================================================
 
-	conn, addr = s.accept()
-	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+# =========== Protocol Constants and Variables ===========
 
-	# ============= Challenge Response Algorithm =============
+INVALID_AUTH_TO_CLIENT = 'HMMM... DISQUALIFIED'
+VALID_AUTH_REQUEST_FROM_CLIENT = 'SHOW ME WHAT YOU GOT'
+challenge = ''
+username = 'asdf'
+challenge_hash = ''
 
-	# 1. Receive "Authentication Request" message.
+# ========================================================
 
-	auth_request = conn.recv(1024)
-	print 'Received Authentication Request: ' + auth_request
+# ========= Challenge Response Algorithm Methods =========
 
-	# 2. Compute and send challenge value (random 64-character string).
+# receive_auth_request receives and verifies the auth request sent by
+#		the client to begin the CRA authentication
+# NOTE: Protocol here is for client to send "SHOW_ME_WHAT_YOU_GOT"
+def receive_auth_request():
+	auth_request = conn.recv(4096)
 
-	challenge = ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(64))
+	# Verify the authentication request
+	if auth_request != VALID_AUTH_REQUEST_FROM_CLIENT:
+		# Client improperly requested auth -- send invalid auth message
+		conn.sendall(INVALID_AUTH_TO_CLIENT)
+		print 'Authentication failed for client' # TODO - Add client details
+
+	if debug:
+		print 'Received Authentication Request: ' + auth_request
+
+# compute_and_send_challenge computes the 64-character random string and sends
+#		it to the client
+# NOTE: Protocol here is for server to send 'challenge=<64-character random string>'
+def compute_and_send_challenge():
+	challenge = 'challenge=' + ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(64))
+	if debug:
+		print 'Sending Challenge: ' + challenge
 	try:
 	    conn.sendall(challenge)
 	except socket.error:
 	    print 'Challenge failed to send.'
 	    sys.exit()
 
+# ========================================================
+
+while 1:
+
+	conn, addr = s.accept()
+	if debug:
+		print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+	# ============= Challenge Response Algorithm =============
+
+	# 1. Receive "Authentication Request" message.
+	receive_auth_request()
+
+	# 2. Compute and send challenge value (random 64-character string).
+	compute_and_send_challenge()
+
 	# 3. Receive username and hash from client.
+	# NOTE: Protocol here is for client to send 'username=<username>&hash=<hash>'
+	md5_hash = conn.recv(4096)
 
-	md5_hash = conn.recv(1024)
-	# TODO - Pull out username from md5_hash
-	username = ''
-	print 'Received MD5 hash: ' + md5_hash
+	# Split the message into arguments
+	arguments = md5_hash.split('&')
 
-	# 4. Find password corresponding to username and perform same MD5 hash. Compare the two hashes.
+	# TODO - Verify order of arguments
 
+	# Validate and trim arguments
+	trimmed_arguments = []
+	expected_arguments = ['username', 'hash']
+	for i in range(len(arguments)):
+		trimmed_arguments.append(trim_argument(arguments[i], expected_arguments[i]))
+
+	if debug:
+		print 'trimmed_arguments: ' + str(trimmed_arguments)
+		print 'len(arguments) = ' + str(len(arguments))
+
+	if len(trimmed_arguments) == 2:
+		username = trimmed_arguments[0]
+		challenge_hash = trimmed_arguments[1]
+		if debug:
+			print 'Received Valid MD5 Hash: ' + md5_hash
+			print 'username: ' + username
+			print 'hash: ' + challenge_hash
+
+	else:
+		# Client sent invalid hash -- send invalid auth message
+		conn.sendall(INVALID_AUTH_TO_CLIENT)
+
+	# 4. Find password corresponding to username and perform same MD5 hash.
+
+	if debug:
+		print 'Finding password for username ' + username
 	password = find_password(username)
+	if debug:
+		print 'Password: ' + password
 	if password == "INVALID_CREDENTIALS":
+		print 'Invalid credentials'
 		# TODO - Send invalid notice to client -- how to restart CRA?
+		# Client improperly requested auth -- send invalid auth message
+		conn.sendall(INVALID_AUTH_TO_CLIENT)
  	else:
 		md5 = hashlib.md5()
 		md5.update(username)
 		md5.update(password)
 		md5.update(challenge)
-		correct_hash = md5.digest()
+		correct_hash = md5.hexdigest()
 
-		# 5. Send appropriate authentication message.
+		# 5. Compare the two hashes. Send the appropriate message to client.
 
-		if md5_hash == correct_hash:
-			print 'Correct hash. Sending authentication.'
-			# TODO - Send authentication
+		if debug:
+			print 'Comparing hashes -- ' + challenge_hash + ' vs ' + correct_hash
+		if challenge_hash == correct_hash:
+			# TODO - Process sensor recording and send the OK to the client
+			conn.sendall('yay you did it')
+			if debug:
+				print 'Correct hash.'
 		else:
-			print 'Incorrect hash. Sending error.'
-			# TODO - Send non-authentication
+			# Client improperly requested auth -- send invalid auth message
+			conn.sendall(INVALID_AUTH_TO_CLIENT)
+			if debug:
+				print 'Incorrect hash.'
+
+	# ========================================================
+
+	# ============== Receiving the Sensor Data ===============
+
+	# NOTE: Protocol here is for client to send 'recording=<recording>'
+	sensor_data = conn.recv(4096)
+	sensor_recording = trim_argument(sensor_data, 'recording')
+	print 'sensor_recording: ' + sensor_recording
+
+	conn.sendall('yo i got your sensor stuff')
+
+	# ========================================================
--- sensor-tcp.py
+++ sensor-tcp.py
@@ -6,22 +6,41 @@ import getopt		# for parsing command-line args
 import hashlib		# for MD5 hash
 
 # Default server, port, username, password, and sensor recording
-server = ''
-port = 80
-username = 'lukenewman'
-password = 'password'
-recording = 0.0
+server = '172.17.0.2'
+port = 1235
+username = 'username1'
+password = 'password1'
+recording = 42.0
+
+# ==================== Helper Methods =====================
+
+# parse_argument will trim the argument from the untrimmed form
+#		i.e. trim_argument('password=1234asdf', 'password') -> '1234asdf'
+def trim_argument(untrimmed, name):
+	# Find and validate '='
+    pos_equals = untrimmed.find("=")
+    if pos_equals == -1: return ''
+
+    # Validate argument name
+    argument_name = untrimmed[0:pos_equals]
+    if name not in argument_name:
+        return ''
+
+	# Return argument after '='
+    return untrimmed[pos_equals + 1:len(untrimmed)]
+
+# ========================================================
 
 # ============= Parse Command-Line Arguments ==============
 
 try:
     opts, args = getopt.getopt(sys.argv[1:],"hs:p:u:c:r:",["server=", "port=", "username=", "password=", "recording="])
 except getopt.GetoptError:
-    print 'Please specify a server (-s), port (-p), username (-u), password (-c), and sensor recording (-r).'
+    print 'Please specify a server (-s), port (-p), username (-u), password (-c), and sensor recording (-r). Use -d for debug printing.'
     sys.exit()
 for opt, arg in opts:
     if opt == '-h':
-        print 'usage: ' + sys.argv[0] + ' -s <server> -p <port> -u <username> -c <password> -r <sensor recording>'
+        print 'Usage: ' + sys.argv[0] + ' -s <server> -p <port> -u <username> -c <password> -r <sensor recording>'
         sys.exit()
     elif opt in ("-s", "--server"):
         server = arg
@@ -39,6 +58,8 @@ for opt, arg in opts:
         recording = float(arg)
         # print 'Recording: ' + arg
 
+# ========================================================
+
 # ======== Create an INET, STREAMing socket (TCP) ========
 
 try:
@@ -48,6 +69,8 @@ except socket.error:
     sys.exit()
 
 print 'Socket created.'
+
+# ========================================================
 
 # ================== Connect to Server ===================
 
@@ -59,37 +82,102 @@ except socket.error, msg:
 
 print 'Socket connected to ' + server + ':' + str(port)
 
+# ========================================================
+
+# =========== Protocol Constants and Variables ===========
+
+INVALID_AUTH_RESPONSE_FROM_SERVER = 'HMMM... DISQUALIFIED'
+AUTH_REQUEST_FOR_SERVER = 'SHOW ME WHAT YOU GOT'
+
+challenge_message = ''
+challenge = ''
+
+# ========================================================
+
 # ============= Challenge Response Algorithm =============
 
-# 1. Send "Authentication Request" message.
+# send_auth_request sends the auth request for the CRA to the server
+# NOTE: Protocol here is for client to send "SHOW_ME_WHAT_YOU_GOT"
+def send_auth_request():
+    try:
+        s.sendall(AUTH_REQUEST_FOR_SERVER)
+    except socket.error:
+        print 'Authentication Request failed to send.'
+        sys.exit()
 
+# receive_challenge receives the challenge string from the server
+# NOTE: Protocol here is for server to send 'challenge=<64-character random string>'
+def receive_challenge():
+    challenge_message = s.recv(4096)
+
+    # Check for invalid authentication message and exit if necessary.
+    if challenge_message == INVALID_AUTH_RESPONSE_FROM_SERVER:
+        print 'User authentication failed!'
+        sys.exit()
+
+    # Extract the challenge string from the message
+    challenge = trim_argument(challenge_message, 'challenge')
+    if len(challenge) != 64:
+        print 'Invalid challenge received. Exiting.'
+        sys.exit()
+    else:
+        print 'Challenge received: ' + challenge
+
+# compute_and_send_challenge_response computes the MD5 hash with hashlib's built-in
+#       functionality, composes the challenge response, and sends it
+# NOTE: Protocol here is for client to send 'username=<username>&hash=<hash>'
+def compute_and_send_challenge_response():
+    md5 = hashlib.md5()
+    md5.update(username)
+    md5.update(password)
+    md5.update(challenge)
+    md5_hash = md5.hexdigest()
+
+    challenge_response = 'username=' + username + '&hash=' + md5_hash
+    print 'Challenge response: ' + challenge_response
+
+    try:
+        s.sendall(challenge_response)
+    except socket.error:
+        print 'Challenge Response failed to send.'
+        sys.exit()
+
+def receive_auth_results():
+    auth_results = s.recv(4096)
+
+    # Alert user of auth results.
+    if auth_results == INVALID_AUTH_RESPONSE_FROM_SERVER:
+        print 'User authentication failed!'
+        sys.exit()
+
+# 1. Send authentication request.
+send_auth_request()
+
+# 2. Receive challenge.
+receive_challenge()
+
+# 3. Compute and send challenge response.
+compute_and_send_challenge_response()
+
+# 4. Receive authentication results.
+receive_auth_results()
+
+# ========================================================
+
+# =============== Sending the Sensor Data ================
+
+print 'sending the sensor data'
+
+# NOTE: Protocol here is for client to send 'recording=<recording>'
+sensor_data = 'recording=' + str(recording)
 try:
-    s.sendall("yo")
+    s.sendall(sensor_data)
 except socket.error:
-    print 'Authentication Request failed to send.'
+    print 'Sensor data failed to send.'
     sys.exit()
 
-# 2. Receive challenge value (random 64-character string).
+sensor_statistics = s.recv(4096)
 
-challenge = s.recv(4096)
-print 'Challenge received: ' + challenge
+print 'Received sensor_statistics: ' + sensor_statistics
 
-# 3. Compute MD5 hash (username, password, challenge)
-
-md5 = hashlib.md5()
-md5.update(username)
-md5.update(password)
-md5.update(challenge)
-md5_hash = md5.digest()
-
-# 4. Send clear text username and hash.
-
-# TODO - Determine protocol for this data (how the server will extract username and hash from data)
-
-try:
-    s.sendall(md5_hash)
-except socket.error:
-    print 'MD5 Hash failed to send.'
-    sys.exit()
-
-# 5. Receive authentication results.
+# ========================================================
